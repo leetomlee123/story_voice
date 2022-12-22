ec0c1f126c450e46c046e542e40bc3eced888f23
Alex <leetomlee123@gmail.com>
2022-12-22 16:46:46 +0800

    init

--- story_voice/settings.py
+++ story_voice/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'story_voice.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'story_voice (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
